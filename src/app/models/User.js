58cb71ddad100c24ce476b91593fc8db94d1e453
XuaanNam <nguyenxuannam0709@gmail.com>
2021-11-06 15:37:57 +0700

    Initial commit

---
+++ src/app/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+var mongoose_delete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const User = new Schema(
+    {
+        _id: Number,
+        userName: { type: String, maxlength: 30 },
+        fullName: { type: String, maxlength: 255 },
+        email: { type: String },
+        image: { type: String },
+        password: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+        // {slug: "name", unique: true} hỗ trợ tự động tạo dựa trên name:{} và không thể trùng
+        //Nếu có 2 name trùng tên nhau, thì nó sẽ tạo ngẫu nhiên 1 short id vào cuối dòng slug
+        //được hỗ trợ bởi mongoose-slug-generator
+    },
+    {
+        _id: false,
+        timestamps: true,
+    } //tự động tạo createdTime & updatedTime
+);
+//add Plugins
+mongoose.plugin(slug);
+User.plugin(mongoose_delete, {
+    deletedAt: true,
+    overrideMethods: 'all',
+});
+User.plugin(AutoIncrement);
+
+module.exports = mongoose.model('User', User);
---
+++ src/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const route = require('./routes');
+const morgan = require('morgan');
+const db = require('./config/db');
+const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
+
+app.use(morgan('combined'));
+
+app.use(express.json());
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+//connect DB
+db.connect();
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        heppers: {
+            sum: (a, b) => a + b, //ở đây ta định nghĩa hàm sum để sử dụng
+
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+                const icon = icons[sortType];
+                const type = types[sort.type];
+                return `<a href="?_sort&column=${field}&type=${type}">
+                            
+                        <span class="${icon}"></span>
+                            </a>`;
+            },
+        },
+    })
+);
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
